deb85e2c0875bc0ba8c5311e40ab4d26a9f73d1d
Sanobelk <63002703+Sanobelk@users.noreply.github.com>
2022-05-03 15:59:48 -0400

    fix bg

--- text-based-game/script.js
+++ text-based-game/script.js
@@ -61,7 +61,7 @@ function toggleMusic(bgm){
 
 scene = document.getElementById("scene");
 
-scene.src = 'images/scenes/town.jpg';
+scene.src = 'https://sanobelk.github.io/text-based-game/images/scenes/town.jpg';
 
 let save = document.getElementById('saveGame');
 
